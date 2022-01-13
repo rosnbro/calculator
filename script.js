6bbc46d6257433b2ed36a76612442b3aa6531973
rosnbro <benro95@gmail.com>
2022-01-13 18:06:15 -0500

    added basic files, introduced basic html structure, added some js basic plans

---
+++ script.js
@@ -0,0 +1,41 @@
+// Constants and Variables
+
+const oneButton = document.querySelector("#one");
+const twoButton = document.querySelector("#two");
+const threeButton = document.querySelector("#three");
+const fourButton = document.querySelector("#four");
+const fiveButton = document.querySelector("#five");
+const sixButton = document.querySelector("#six");
+const sevenButton = document.querySelector("#seven");
+const eightButton = document.querySelector("#eight");
+const nineButton = document.querySelector("#nine");
+const zeroButton = document.querySelector("#zero");
+const deciButton = document.querySelector("#decimal");
+const devButton = document.querySelector("#divide");
+const multiButton = document.querySelector("#multiply");
+const subButton = document.querySelector("#subtract");
+const addButton = document.querySelector("#add");
+const equalButton = document.querySelector("#equal");
+const clearButton = document.querySelector("#clear");
+const allClearButton = document.querySelector("#allClear");
+
+// possible plan of attack
+// Get input from buttons/keyboard into the display value as a string, 
+// then convert to number or float depending on decimal. convert upon action 
+// taken by pressing an operation button
+
+function add() {
+
+}
+
+function subtract() {
+
+}
+
+function multiply() {
+
+}
+
+function divide() {
+
+}

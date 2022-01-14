0c9b7e3f5131b930a912ff2d8ae90fef39c6af59
rosnbro <benro95@gmail.com>
2022-01-13 22:04:57 -0500

    added event listeners to number and decimal buttons

--- script.js
+++ script.js
@@ -1,16 +1,5 @@
 // Constants and Variables
-
-const oneButton = document.querySelector("#one");
-const twoButton = document.querySelector("#two");
-const threeButton = document.querySelector("#three");
-const fourButton = document.querySelector("#four");
-const fiveButton = document.querySelector("#five");
-const sixButton = document.querySelector("#six");
-const sevenButton = document.querySelector("#seven");
-const eightButton = document.querySelector("#eight");
-const nineButton = document.querySelector("#nine");
-const zeroButton = document.querySelector("#zero");
-const deciButton = document.querySelector("#decimal");
+/*
 const devButton = document.querySelector("#divide");
 const multiButton = document.querySelector("#multiply");
 const subButton = document.querySelector("#subtract");
@@ -18,6 +7,17 @@ const addButton = document.querySelector("#add");
 const equalButton = document.querySelector("#equal");
 const clearButton = document.querySelector("#clear");
 const allClearButton = document.querySelector("#allClear");
+*/
+
+const numButtons = document.querySelectorAll(".numButton");
+
+let buttonSelection;
+
+numButtons.forEach((button) => {
+    button.addEventListener("click", () => {
+        buttonSelection = button.textContent
+    })
+});
 
 // possible plan of attack
 // Get input from buttons/keyboard into the display value as a string, 

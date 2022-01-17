f87464c9dc02c2db00b81a98f5dd3184e5c74211
rosnbro <benro95@gmail.com>
2022-01-17 15:55:43 -0500

    fixed logic to empty the correct values when expected. Began setting up basic style

--- script.js
+++ script.js
@@ -1,29 +1,28 @@
 // Constants and Variables
-/*
-const allClearButton = document.querySelector("#allClear");
-*/
 
-const numOneDisplay = document.querySelector(".numOne");
-const operatorDisplay = document.querySelector(".operator")
-const numTwoDisplay = document.querySelector(".numTwo");
-const activeNumDispaly = document.querySelector(".activeNum")
+const numOneDisplay = document.querySelector("#numOne");
+const operatorDisplay = document.querySelector("#operator")
+const numTwoDisplay = document.querySelector("#numTwo");
+const activeNumDispaly = document.querySelector("#activeNum")
 const numButtons = document.querySelectorAll(".numButton");
 const opButtons = document.querySelectorAll(".operator");
 const deciButton = document.querySelector("#decimal");
-const backButton = document.querySelector("#backspace");
+const signButton = document.querySelector("#sign");
 const equalButton = document.querySelector("#equal");
+const allClearButton = document.querySelector("#allClear");
+const backButton = document.querySelector("#backspace");
 
 let displayValue = "";
-let operator;
-let num1;
-let num2;
+let operator = null;
+let num1 = null;
+let num2 = null;
 
 // Event listeners
 
 document.addEventListener("keydown", (e) => {
     if (e.key >= 0 && e.key <= 9 || e.key == ".") {
         displayValue += e.key;
-        display(displayValue, num1, num2, operatorSymbol(operator));
+        display(displayValue, num1, num2, operator);
     } else if (e.key == "Enter") {
         if (num1 != null && displayValue != "") calculate(num1, displayValue);
     } else if (e.key == "Backspace") {
@@ -33,16 +32,17 @@ document.addEventListener("keydown", (e) => {
 
 numButtons.forEach((numButton) => {
     numButton.addEventListener("click", () => {
-        displayValue += numButton.textContent
-        display(displayValue, num1, num2, operatorSymbol(operator));
-    })
+        if (num2 != null && displayValue != "") clearAll();
+        displayValue += numButton.textContent;
+        display(displayValue, num1, num2, operator);
+    });
 });
 
 opButtons.forEach((opButton) => {
     opButton.addEventListener("click", () => {
-        operator = opButton.id
-        assignNumber()
-    })
+        operator = opButton.id;
+        assignNumber();
+    });
 });
 
 deciButton.addEventListener("click", () => {
@@ -50,77 +50,109 @@ deciButton.addEventListener("click", () => {
         if (displayValue == "") displayValue += 0;
         displayValue += ".";
     }
-    display(displayValue, num1, num2, operatorSymbol(operator));
+    display(displayValue, num1, num2, operator);
 });
 
-backButton.addEventListener("click", () => backSpace(displayValue));
+signButton.addEventListener("click", () => {
+    displayValue = "-" + displayValue;
+    display(displayValue, num1, num2, operator);
+});
 
 equalButton.addEventListener("click", () => {
     if (num1 != null && displayValue != "") calculate(num1, displayValue);
 });
 
+allClearButton.addEventListener("click", () => clearAll());
+
+backButton.addEventListener("click", () => backSpace(displayValue));
+
+
+
 // Add ability to use shift+num operators when typed
-display(displayValue, num1, num2, operatorSymbol(operator));
+
+display(displayValue, num1, num2, operator);
 
 // Functions
 
 function display(active, num1, num2, operator) {
-    if (active == null) activeNumDispaly.innerHTML = "";
-    if (num1 == null) numOneDisplay.innerHTML = "";
-    if (num2 == null) numTwoDisplay.innerHTML = "";
-    if (operator == null) operatorDisplay.innerHTML = "";
+    //if (active === null) activeNumDispaly.innerHTML = "";
+    //if (num1 === null) numOneDisplay.innerHTML = "";
+    //if (num2 === null) numTwoDisplay.innerHTML = "";
+    //if (operator === null) operatorDisplay.innerHTML = "";
+
+    let operatorSymbol;
+
+    switch (operator) {
+        case "add":
+            operatorSymbol = "+";
+            break;
+        case "subtract":
+            operatorSymbol = "-";
+            break;
+        case "multiply":
+            operatorSymbol = "*";
+            break;
+        case "divide":
+            operatorSymbol = "&#247";
+            break;
+        case "pow":
+            operatorSymbol = "^";
+            break;
+        case null:
+            operatorSymbol = null;
+            break;
+    }
 
     activeNumDispaly.innerHTML = active;
     numOneDisplay.innerHTML = num1;
     numTwoDisplay.innerHTML = num2;
-    operatorDisplay.innerHTML = operator;
+    operatorDisplay.innerHTML = operatorSymbol;
 }
 
 function assignNumber() {
-    if (num2 != null) num2 = "";
-    num1 = parseFloat(displayValue);
-    displayValue = "";
-    display(displayValue, num1, num2, operatorSymbol(operator));
+    if (num2 != null) num2 = null;
+    if (displayValue == "") {
+        display(displayValue, num1, num2, operator);
+    } else {
+        num1 = parseFloat(displayValue);
+        displayValue = "";
+        display(displayValue, num1, num2, operator);
+    }
 }
 
 function backSpace(value) {
     displayValue = value.slice(0, -1);
-    display(displayValue, num1, num2, operatorSymbol(operator));
+    display(displayValue, num1, num2, operator);
+}
+
+function clearAll() {
+    displayValue = "";
+    operator = null;
+    num1 = null;
+    num2 = null;
+    display(displayValue, num1, num2, operator);
 }
 
 function calculate() {
-    let num2 = parseFloat(displayValue);
+    num2 = parseFloat(displayValue);
     switch (operator) {
         case "add":
             displayValue = add(num2);
-            display(displayValue, num1, num2, operatorSymbol(operator));
             break;
         case "subtract":
             displayValue = subtract(num2);
-            display(displayValue, num1, num2, operatorSymbol(operator));
             break;
         case "multiply":
             displayValue = multiply(num2);
-            display(displayValue, num1, num2, operatorSymbol(operator));
             break;
         case "divide":
             displayValue = divide(num2);
-            display(displayValue, num1, num2, operatorSymbol(operator));
+            break;
+        case "pow":
+            displayValue = power(num2);
             break;
     }
-}
-
-function operatorSymbol(operator) {
-    switch (operator) {
-        case "add":
-            return "+";
-        case "subtract":
-            return "-";
-        case "multiply":
-            return "x";
-        case "divide":
-            return "&#247";
-    }
+    display(displayValue, num1, num2, operator);
 }
 
 function add(num2) {
@@ -141,6 +173,10 @@ function divide(num2) {
     } else return "lol nope"
 }
 
+function power(num2) {
+    return Math.pow(num1, num2);
+}
+
 function squareRoot() {
 
 }
@@ -149,6 +185,13 @@ function factorial() {
 
 }
 
-function exp() {
+
+
+function naturalLog() {
 
 }
+
+function commonLog() {
+
+}
+
